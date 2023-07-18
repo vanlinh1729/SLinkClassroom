32f09dd53287a289866ad3ff1b803a3b7c1e8fab
Linh <vanlinh1729@gmail.com>
2023-07-18 17:21:28 +0700

    Connect client v1

--- SuperLinkClassroom/MainWindow.xaml.cs
+++ SuperLinkClassroom/MainWindow.xaml.cs
@@ -58,6 +58,8 @@ namespace SuperLinkClassroom
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
+
             for (int i = 0; i < ips.Count; i++)
             {
                 IPHostEntry hostEntry = Dns.GetHostEntry(ips[i]);
@@ -150,7 +152,6 @@ namespace SuperLinkClassroom
                 txtConnect.Text = "Connect";
                 btn_Connect.IsEnabled = true;
                 btn_Disconnect.IsEnabled = false;
-                ips.Clear();
             }
             catch (Exception ex)
             {
@@ -222,7 +223,6 @@ namespace SuperLinkClassroom
         {
             try
             {
-                ips.Clear();
                 string ipAddress = txtIPMask.Text;
                 for (int i = 1; i <= 255; i++)
                 {
@@ -231,13 +231,240 @@ namespace SuperLinkClassroom
                     ping.PingCompleted += PingCompleted;
                     ping.SendAsync(currentIP, 100, currentIP);
                 }
+
+                btn_Search.IsEnabled = false;
             }
             catch (Exception exc)
             {
                 if (ips.Count == 0)
-                    MessageBox.Show(exc.Message+" LAN have no Client!. Check your client or your LAN connection.");
+                    MessageBox.Show("LAN have no Client!. Check your client or your LAN connection.");
+                btn_Search.IsEnabled = true;
             }
 
         }
     }
 }
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Net;
+// using System.Net.NetworkInformation;
+// using System.Net.Sockets;
+// using System.Threading.Tasks;
+// using System.Windows;
+// using System.Windows.Controls;
+// using System.Windows.Data;
+// using System.Windows.Documents;
+// using System.Windows.Input;
+// using System.Windows.Media;
+// using System.Windows.Media.Imaging;
+// using MaterialDesignThemes.Wpf;
+// using VncSharpWpf;
+//
+// namespace SuperLinkClassroom
+// {
+//     public partial class MainWindow : Window
+//     {
+//         private List<string> ips = new List<string>();
+//         private int connectedCount = 0;
+//
+//         public MainWindow()
+//         {
+//             InitializeComponent();
+//             UpdateConnectedCount();
+//             btn_Disconnect.IsEnabled = false;
+//             btn_Connect.IsEnabled = true;
+//         }
+//
+//         private async void MainWindow_OnLoad(object sender, RoutedEventArgs e)
+//         {
+//             WindowStartupLocation = WindowStartupLocation.CenterScreen;
+//             WindowState = WindowState.Maximized;
+//             Activate();
+//
+//             // await SearchForIPAddressesAsync();
+//         }
+//
+//         private async void Button_Click(object sender, RoutedEventArgs e)
+//         {
+//             foreach (string ipAddress in ips)
+//             {
+//                 GroupBox groupBox = CreateGroupBox(ipAddress);
+//                 RemoteDesktopWpf rdp = new RemoteDesktopWpf();
+//                 rdp.Name = "rdp" + (connectionPage.Children.Count + 1);
+//                 groupBox.Content = rdp;
+//
+//                 try
+//                 {
+//                     rdp.Connect(ipAddress);
+//                     rdp.SetInputMode(true);
+//                     connectedCount++;
+//                 }
+//                 catch (Exception exception)
+//                 {
+//                     MessageBox.Show(exception.Message, "Error when connecting");
+//                 }
+//
+//                 connectionPage.Children.Add(groupBox);
+//             }
+//
+//             btn_Connect.IsEnabled = false;
+//             btn_Disconnect.IsEnabled = true;
+//             UpdateConnectedCount();
+//         }
+//
+//         private async void Button_Click_1(object sender, RoutedEventArgs e)
+//         {
+//             try
+//             {
+//                 foreach (GroupBox groupBox in connectionPage.Children.OfType<GroupBox>())
+//                 {
+//                     RemoteDesktopWpf rdp = groupBox.Content as RemoteDesktopWpf;
+//                     if (rdp != null)
+//                     {
+//                         rdp.Disconnect();
+//                     }
+//                 }
+//
+//                 MessageBox.Show("All connections disconnected");
+//                 btn_Connect.IsEnabled = true;
+//                 btn_Disconnect.IsEnabled = false;
+//                 ips.Clear();
+//                 connectedCount = 0;
+//             }
+//             catch (Exception ex)
+//             {
+//                 MessageBox.Show(ex.Message);
+//             }
+//
+//             connectionPage.Children.Clear();
+//             UpdateConnectedCount();
+//         }
+//
+//         private void UpdateConnectedCount()
+//         {
+//             txtConnectedCount.Text = connectedCount.ToString();
+//         }
+//
+//         private async Task SearchForIPAddressesAsync()
+//         {
+//             string ipAddress = txtIPMask.Text;
+//
+//             await Task.Run(() =>
+//             {
+//                 Parallel.For(1, 255, (i) =>
+//                 {
+//                     string currentIP = $"{ipAddress}{i}";
+//                     Ping ping = new Ping();
+//                     ping.PingCompleted += PingCompleted;
+//                     ping.SendAsync(currentIP, 100, currentIP);
+//                 });
+//             });
+//
+//             btn_Search.IsEnabled = false;
+//         }
+//
+//         private void PingCompleted(object sender, PingCompletedEventArgs e)
+//         {
+//             try
+//             {
+//                 if (e.Reply != null && e.Reply.Status == IPStatus.Success)
+//                 {
+//                     if (IsOpenPort(e.Reply.Address.ToString()))
+//                     {
+//                         ips.Add(e.Reply.Address.ToString());
+//                     }
+//                 }
+//             }
+//             catch (Exception exception)
+//             {
+//                 Console.WriteLine(exception);
+//             }
+//         }
+//
+//         private bool IsOpenPort(string ip)
+//         {
+//             using (TcpClient client = new TcpClient())
+//             {
+//                 try
+//                 {
+//                     client.Connect(ip, 5900);
+//                     return true;
+//                 }
+//                 catch (Exception)
+//                 {
+//                     return false;
+//                 }
+//             }
+//         }
+//
+//         private GroupBox CreateGroupBox(string ipAddress)
+//         {
+//             IPHostEntry hostEntry = Dns.GetHostEntry(ipAddress);
+//             string platform = hostEntry.HostName;
+//
+//             GroupBox groupBox = new GroupBox()
+//             {
+//                 Header = $"{platform} ({ipAddress})",
+//                 Height = 250,
+//                 Width = 360,
+//                 Margin = new Thickness(0, 30, 20, 0),
+//                 Style = (Style)Application.Current.Resources["MaterialDesignCardGroupBox"]
+//             };
+//
+//             ColorZoneAssist.SetBackground(groupBox, new SolidColorBrush(Colors.DimGray));
+//             ColorZoneAssist.SetForeground(groupBox, new SolidColorBrush(Colors.White));
+//             ColorZoneAssist.SetMode(groupBox, ColorZoneMode.Custom);
+//
+//             DataTemplate headerTemplate = new DataTemplate();
+//             FrameworkElementFactory stackPanelFactory = new FrameworkElementFactory(typeof(StackPanel));
+//             stackPanelFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+//
+//             FrameworkElementFactory packIconFactory = new FrameworkElementFactory(typeof(PackIcon));
+//             packIconFactory.SetValue(PackIcon.WidthProperty, 32.0);
+//             packIconFactory.SetValue(PackIcon.HeightProperty, 32.0);
+//             packIconFactory.SetValue(PackIcon.VerticalAlignmentProperty, VerticalAlignment.Center);
+//             packIconFactory.SetValue(PackIcon.KindProperty, PackIconKind.Monitor);
+//
+//             FrameworkElementFactory textBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+//             textBlockFactory.SetValue(TextBlock.MarginProperty, new Thickness(8, 0, 0, 0));
+//             textBlockFactory.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+//             textBlockFactory.SetValue(TextBlock.StyleProperty,
+//                 Application.Current.Resources["MaterialDesignSubtitle1TextBlock"]);
+//             textBlockFactory.SetBinding(TextBlock.TextProperty, new Binding());
+//
+//             stackPanelFactory.AppendChild(packIconFactory);
+//             stackPanelFactory.AppendChild(textBlockFactory);
+//
+//             headerTemplate.VisualTree = stackPanelFactory;
+//             groupBox.HeaderTemplate = headerTemplate;
+//
+//             return groupBox;
+//         }
+//
+//         private async void Btn_Save_OnClick(object sender, RoutedEventArgs e)
+//         {
+//             if (txtSave.Text == "Save")
+//             {
+//                 txt_IPMask.IsEnabled = false;
+//                 txtSave.Text = "Edit";
+//                 iconSaveEdit.Kind = PackIconKind.Edit;
+//             }
+//             else
+//             {
+//                 txt_IPMask.IsEnabled = true;
+//                 txtSave.Text = "Save";
+//                 iconSaveEdit.Kind = PackIconKind.ContentSave;
+//             }
+//
+//             await SearchForIPAddressesAsync();
+//         }
+//
+//         private async void Btn_Search_OnClick(object sender, RoutedEventArgs e)
+//         {
+//             await SearchForIPAddressesAsync();
+//         }
+//     }
+// }
