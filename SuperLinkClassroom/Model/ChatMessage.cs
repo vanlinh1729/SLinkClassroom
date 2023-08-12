381b56af7945d67c46d1f98d2e4bccd0f51d2c76
Linh <vanlinh1729@gmail.com>
2023-08-12 16:09:38 +0700

    Chat ok

---
+++ SuperLinkClassroom/Model/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperLinkClassroom.Model
+{
+    public class ChatMessage
+    {
+        public string SenderInfo { get; set; } // Tên PC và địa chỉ IP
+        public string MessageContent { get; set; } // Nội dung tin nhắn
+    }
+
+}
